553f692175895fa944a777d4d724dd943d01e125
LeungZ <31878600+LeungZ9@users.noreply.github.com>
2017-09-14 20:42:18 +0800

    Angular echarts lite directive commit

--- src/angular-echarts-lite.js
+++ src/angular-echarts-lite.js
@@ -3,28 +3,81 @@
  */
 import angular from 'angular';
 import echarts from 'echarts';
-import controller from './controller';
 
+/**
+ * Angular directive
+ */
 class Directive {
     constructor() {
+        /** @private  {boolean}*/
         this.replace = true;
+        /** @private  {string}*/
         this.restrict = 'E';
+        /** @private  {scope}*/
         this.scope = {
-            model:'=',
             config: '=',
-            theme: '='
+            instance: '=?',
+            theme: '=?'
         };
+        /** @private  {string}*/
         this.template = '<div></div>';
-        this.controller = controller;
     }
 
-    link(scope, element, attr, ctrl) {
-        ctrl;
-        echarts;
-        // To do
+    /** @private  */
+    link(scope, element, attr) {
+        /**
+         * @param {echartsOptions} options
+         * @private
+         */
+        function setOptions(options) {
+            options = options || {};
+            options.forceClear && chart.clear();
+            if (options.series && options.series.length) {
+                chart.hideLoading();
+                chart.setOption(options, options.notMerge);
+                chart.resize();
+            } else {
+                chart.showLoading('default', options.errorMsg || { text: '没有数据' });
+            }
+        }
+
+        var chart = echarts.init(element[0], scope.theme);
+        setOptions(scope.config);
+
+        // Check for attribute "unwatch" whether to register scope watch
+        if (!attr.hasOwnProperty('unwatch')) {
+            scope.$watch(function () {
+                return scope.config;
+            }, function (newValue, oldValue) {
+                if (newValue !== oldValue) {
+                    setOptions(newValue);
+                }
+            }, true);
+            scope.$watch(function () {
+                return scope.theme;
+            }, function (newValue, oldValue) {
+                if (newValue !== oldValue) {
+                    const options = chart.getOption();
+                    chart.dispose();
+                    chart = echarts.init(element[0], newValue);
+                    chart.setOption(options);
+                }
+            }, true);
+        }
+
+        // Check for attribute "instance" whether to bind function
+        // to get echarts instance
+        if (attr.instance) {
+            scope.$evalAsync(function () {
+                scope.instance = /** @return {echartsInstance} */ function () {
+                    return chart;
+                };
+            });
+        }
     }
 
-    static factory() {
+    /** @return {directive } */
+    static get factory() {
         return new Directive();
     }
 }
