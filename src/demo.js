d91a96adc2d6be7b83b69228d6bc263b0b7b332f
LeungZ <31878600+LeungZ9@users.noreply.github.com>
2017-09-21 13:48:23 +0800

    Update demo.js

--- src/demo.js
+++ src/demo.js
@@ -1,4 +1,74 @@
 import angular from 'angular';
+import echarts from 'echarts';
+import 'echarts/theme/vintage';
+import 'echarts/theme/dark';
+import 'echarts/theme/macarons';
 import 'angular-echarts-lite';
 
-angular.module('demo', ['angular-echarts-lite']);
+angular.module('demo', ['angular-echarts-lite'])
+    /*eslint angular/file-name: [off]*/
+    .controller('DemoController', ['$timeout', function ($timeout) {
+        var timeout, chartInst, vm = this;
+        
+        vm.config = { //Init echarts option
+            title: {
+                text: '对数轴示例',
+                left: 'center'
+            },
+            tooltip: {
+                trigger: 'item',
+                formatter: '{a} <br/>{b} : {c}'
+            },
+            legend: {
+                left: 'left',
+                data: ['3的指数']
+            },
+            xAxis: {
+                type: 'category',
+                name: 'x',
+                splitLine: { show: false },
+                data: ['一', '二', '三', '四', '五', '六', '七', '八', '九']
+            },
+            grid: {
+                left: '3%',
+                right: '4%',
+                bottom: '3%',
+                containLabel: true
+            },
+            yAxis: {
+                type: 'log',
+                name: 'y'
+            },
+            series: [
+                {
+                    name: '3的指数',
+                    type: 'line',
+                    data: [1, 3, 9, 27, 81, 247, 741, 2223, 6669]
+                }
+            ]
+        };
+        vm.configTxt = angular.toJson(vm.config, true);
+
+        vm.configTxtChange = function(){
+            $timeout.cancel(timeout);
+            timeout = $timeout((function(){
+                vm.config = angular.fromJson(vm.configTxt);
+                // If not register scope watcher, update chart via API on chart instance
+                if(vm.unwatch){
+                    if(!chartInst){
+                        chartInst = vm.chartInst();
+                    }
+                    chartInst.setOption(vm.config);
+                }
+            }).bind(vm), 300);
+        };
+
+        // If not register scope watcher, update chart via API on chart instance
+        vm.themeChange = function(){
+            if(vm.unwatch){
+                chartInst.dispose();
+                chartInst = echarts.init(chartInst.getDom(), vm.theme);
+                chartInst.setOption(vm.config);
+            }
+        };
+    }]);
--- webpack.config.js
+++ webpack.config.js
@@ -5,43 +5,26 @@
 const webpack = require('webpack');
 const path = require('path');
 
-const IS_DEV = process.env.npm_lifecycle_event === "dev";
-
 module.exports = {
     context: path.resolve('./src'),
     entry: path.resolve("./src/demo"),
     output: {
-        path: path.resolve('./dist'),
         filename: 'demo.min.js'
     },
     externals: {
-        angular: "angular"
+        angular: "angular",
+        echarts: "echarts"
     },
     devServer: {
         disableHostCheck: true,
     },
-    devtool: IS_DEV ? 'source-map' : false,
+    devtool: false,
     module: {
         rules: [
             {
                 test: /\.js$/,
-                exclude: /node_modules/,
-                use: [
-                    {
-                        loader: "babel-loader",
-                        options: {
-                            presets: ["es2015"]
-                        }
-                    }, {
-                        loader: "eslint-loader"
-                    }
-                ]
+                exclude: /node_modules/
             }
         ]
-    },
-    plugins: [
-        new webpack.optimize.UglifyJsPlugin({
-            include: /\.min\.js$/
-        })
-    ]
+    }
 };
